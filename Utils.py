55fc105ab167ca55c00916bb9e2593d0c6c0ef12
yc <flashfoxmx@gmail.com>
2024-03-05 05:45:48 -0800

    entropy

---
+++ Utils.py
@@ -0,0 +1,153 @@
+import os
+import numpy as np
+from numba import njit
+
+# Const ordered dithering matrices
+# To use decorator numba.njit, matrices must be hard-coded separately
+mat2 = np.asarray([[0, 2], [3, 1]])
+mat4 = np.asarray([[0, 8, 2, 10], [12, 4, 14, 6], [3, 11, 1, 9], [15, 7, 13, 5]])
+mat8 = np.asarray([[ 0, 32,  8, 40,  2, 34, 10, 42],
+                   [48, 16, 56, 24, 50, 18, 58, 26],
+                   [12, 44,  4, 36, 14, 46,  6, 38],
+                   [60, 28, 52, 20, 62, 30, 54, 22],
+                   [ 3, 35, 11, 43,  1, 33,  9, 41],
+                   [51, 19, 59, 27, 49, 17, 57, 25],
+                   [15, 47,  7, 39, 13, 45,  5, 37],
+                   [63, 31, 55, 23, 61, 29, 53, 21]])
+
+def readBMP(fileName: str) -> (np.ndarray, (int, int), str):
+    # read file and check validity
+    try:
+        file = open(fileName, 'rb')
+    except IOError:
+        return None, (0, 0), str.format("File not found")
+    try:
+        bmpMarker = file.read(2)
+        fileSize = int.from_bytes(file.read(4), "little")
+        file.seek(12, os.SEEK_CUR)
+        width = int.from_bytes(file.read(4), "little")
+        height = int.from_bytes(file.read(4), "little")
+        file.seek(2, os.SEEK_CUR)
+        channel = int.from_bytes(file.read(2), "little")
+        assert bmpMarker == b'BM', str.format("Not a BMP file")
+        assert fileSize > 0 and width > 0 and height > 0 and channel > 0, str.format("BMP file not valid")
+    except AssertionError as e:
+        return None, (-1, -1), str(e)
+    # read pixel data
+    file.seek(24, os.SEEK_CUR)
+    data = np.zeros((height, width, 3), dtype=np.uint8)
+    for i in range(height - 1, -1, -1):
+        for j in range(width):
+            bB = int.from_bytes(file.read(1), "little", signed=False)
+            bG = int.from_bytes(file.read(1), "little", signed=False)
+            bR = int.from_bytes(file.read(1), "little", signed=False)
+            data[i, j] = np.array([bR, bG, bB], dtype=np.uint8)
+        # eat up padding 0s in raw data
+        for k in range(width % 4):
+            r = int.from_bytes(file.read(1), "little", signed=False)
+    return data, (width, height), ""
+
+@njit
+def cvtGrayscale(data: np.ndarray) -> np.ndarray:
+    # returned data will not be 32-aligned
+    ret = np.zeros((data.shape[0], data.shape[1], 1), dtype=np.uint8)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            val = 0.299 * data[i, j, 0] + 0.587 * data[i, j, 1] + 0.114 * data[i, j, 2]
+            # Always round 0.5 up; carefully deal with case like 0.4999999 (should be 0.5)
+            if (val % 1) >= 0.5:
+                val = np.floor(val) + 1
+            elif 0.5 - (val % 1) < 1e-6:
+                val = np.floor(val) + 1
+            else:
+                val = np.floor(val)
+            ret[i, j, 0] = int(val)
+    return ret
+
+@njit
+def cvtAlignedData(data: np.ndarray) -> np.ndarray:
+    # dealing with 32-alignment issue, padding (4 - width) % 4 0s
+    ret = np.zeros((data.shape[0], data.shape[1] + (4 - data.shape[1]) % 4, 1), dtype=np.uint8)
+    ret[0: data.shape[0], 0: data.shape[1]] = data
+    return ret
+
+@njit
+def cvtOrderedDithering(data: np.ndarray, ditType: int = 0) -> np.ndarray:
+    DIM = 2 ** (ditType + 1) # dimension of dithering matrix
+    MAX = DIM ** 2 - 1 # maximum value of dithering matrix
+    ret = np.zeros(data.shape, dtype=np.uint8)
+    match ditType:
+        case 0:
+            mat = mat2
+        case 1:
+            mat = mat4
+        case _:
+            mat = mat8
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            for k in range(data.shape[2]):
+                x, y = i % DIM, j % DIM
+                ret[i, j, k] = 255 if data[i, j, k] * MAX / 255 > mat[x, y] else 0
+    return ret
+
+@njit
+def histogram(data: np.ndarray) -> np.ndarray:
+    ret = np.zeros((data.shape[2], 256), dtype=np.uint32)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            for k in range(data.shape[2]):
+                ret[k, data[i, j, k]] += 1
+    return ret
+
+def calEntropy(histogram: np.ndarray) -> np.ndarray:
+    ret = np.zeros((histogram.shape[0], 1), dtype=np.float32)
+    total = np.sum(histogram[0])
+    for channel in range(histogram.shape[0]):
+        for count in range(1, histogram.shape[1]):
+            p = float(histogram[channel, count] / total)
+            ret[channel] -= p * np.emath.log2(max(p, 1e-10))
+    return ret
+
+@njit
+def colorAdjustment(rawData: np.ndarray,
+                    channel: int = 0,
+                    inSlider: (int, float, int) = (0, 1.0, 255), # inSlider = (low level, gamma, high level)
+                    outSlider: (int, int) = (0, 255), # outSlider = (low level, high level)
+                    ) -> np.ndarray:
+    ret = np.copy(rawData)
+    for i in range(rawData.shape[0]):
+        for j in range(rawData.shape[1]):
+            if rawData[i, j, channel] <= inSlider[0]:
+                pixel = 0.0
+            elif rawData[i, j, channel] >= inSlider[2]:
+                pixel = 1.0
+            else:
+                pixel = (rawData[i, j, channel] - inSlider[0]) / (inSlider[2] - inSlider[0])
+            corrected = np.power(pixel, 1.0 / inSlider[1])
+            ret[i, j, channel] = int(corrected * (outSlider[1] - outSlider[0]) + outSlider[0])
+    return ret
+
+'''
+Normalize values in data to given range
+'''
+@njit
+def normalize(data: np.ndarray, outRange: (int, int) = (0, 255)) -> np.ndarray:
+    ret = np.zeros(data.shape, dtype=np.uint8)
+    for k in range(data.shape[2]):
+        low, high = np.min(data[:, :, k]), np.max(data[:, :, k])
+        for i in range(data.shape[0]):
+            for j in range(data.shape[1]):
+                if data[i, j, k] == low:
+                    ret[i, j, k] = outRange[0]
+                elif data[i, j, k] == high:
+                    ret[i, j, k] = outRange[1]
+                else:
+                    ret[i, j, k] = int((data[i, j, k] - low) / (high - low) * (outRange[1] - outRange[0])) + outRange[0]
+    return ret
+
+'''
+Histogram equalization: 
+https://en.wikipedia.org/wiki/Histogram_equalization
+'''
+def histogramEqualization(data: np.ndarray) -> np.ndarray:
+    return None
---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PSWindow import PSWindow
+try:
+    CUSTOMTHEME = True
+    import qdarktheme
+except ModuleNotFoundError:
+    CUSTOMTHEME = False
+
+if __name__ == '__main__':
+    mainApp = QApplication(sys.argv)
+    if CUSTOMTHEME:
+        qdarktheme.setup_theme(custom_colors={"background": "#404040"})
+    psWindow = PSWindow()
+    psWindow.show()
+    sys.exit(mainApp.exec_())
